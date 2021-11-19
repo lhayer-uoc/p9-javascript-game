23a800f32832db149eac5a39e8dbc1c7fc7a6b92
LHayer-UOC <lhayer@uoc.edu>
2021-11-19 23:51:50 +0100

    refactor play screen

--- src/public/js/play.js
+++ src/public/js/play.js
@@ -6,6 +6,9 @@ $(document).ready(async function () {
     console.log(game);
 
     renderPlayers(game);
+    renderBoard();
+    renderPlayerFirstCell(game);
+    setBackNavigation();
   }
 
   // FETCH FUNCTIONS
@@ -32,6 +35,11 @@ $(document).ready(async function () {
   }
 
   // RENDER FUNCTIONS
+
+  function showUserContainer(idx) {
+    const userContainer = $(`#player-${idx}`).get(0);
+    $(userContainer).toggleClass('hidden');
+  }
 
   function paintUserLogo(image, idx) {
     const imageList = document.querySelectorAll(`#player-${idx} .user-logo`);
@@ -72,6 +80,7 @@ $(document).ready(async function () {
   }
 
   function renderUser(user, playerData, idx) {
+    showUserContainer(idx);
     paintUserLogo(user.image, idx);
     paintUserName(user.name, idx);
     paintUserColor(user.color, idx);
@@ -87,5 +96,65 @@ $(document).ready(async function () {
         idx++;
       }
     }
+  }
+
+  function createBoardRow() {
+    return $('<div>', { class: 'flex flex-rows board-row' });
+  }
+
+  function createBoardCell(i, j) {
+    return $('<div>', {
+      class: 'flex flex-rows board-cell',
+      'data-row': i,
+      'data-col': j,
+    });
+  }
+
+  function renderBoard() {
+    const board = $('#game-board').get(0);
+    $(board).on('click', takeCell);
+    for (let i = 0; i < 20; i++) {
+      const row = createBoardRow();
+      $(row).appendTo(board);
+      for (let j = 0; j < 20; j++) {
+        const cell = createBoardCell(i, j);
+        $(cell).appendTo(row);
+      }
+    }
+  }
+
+  function renderPlayerFirstCell(game) {}
+
+  // PLAY LOGIC
+  function getCellTarget(rowId, colId) {
+    return $(`[data-row=${rowId}][data-col=${colId}]`).get(0);
+  }
+
+  function getUserColor() {
+    const userStringified = localStorage.getItem('user');
+    if (userStringified) {
+      const user = JSON.parse(userStringified);
+      return user.color ? user.color : '#000';
+    }
+  }
+
+  function takeCell(event) {
+    const cellTarget = event.target;
+    const rowId = $(cellTarget).attr('data-row');
+    const colId = $(cellTarget).attr('data-col');
+    const cell = getCellTarget(rowId, colId);
+    const userColor = getUserColor();
+    $(cell).css('background-color', userColor);
+  }
+
+  // NAVIGATION
+
+  function setBackNavigation() {
+    const backpathButton = $('#backpath').get(0);
+    $(backpathButton).on('click', navigateHome);
+  }
+
+  function navigateHome() {
+    window.location.replace('/room-game');
   }
 });
